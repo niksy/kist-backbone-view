26bd5de82da5b980966684edd606eaae9b3d4234
Ivan Nikolić <niksy5@gmail.com>
2017-05-01 11:33:59 +0200

    Improve tests

--- test/index.js
+++ test/index.js
@@ -4,55 +4,72 @@ var View = require('../index');
 
 describe('Basic', function () {
 
-	var view;
-
 	it('should have instance with `getSubview`, `addSubview` and `removeSubviews` methods', function () {
-		view = new View();
+		var view = new View();
 		assert.equal(typeof view.getSubview, 'function');
 		assert.equal(typeof view.addSubview, 'function');
 		assert.equal(typeof view.removeSubviews, 'function');
+		view.remove();
 	});
 
 	it('should have instance with 3 subviews after adding them', function () {
+		var view = new View();
 		view.addSubview(new View());
 		view.addSubview(new View());
 		view.addSubview(new View(), 'customKey');
 		assert.equal(Object.keys(view.subviews).length, 3);
+		view.remove();
 	});
 
 	it('should have instance with 1 subview with key "customKey"', function () {
+		var view = new View();
+		view.addSubview(new View(), 'customKey');
 		assert.equal(view.getSubview('customKey') instanceof View, true);
+		view.remove();
 	});
 
 	it('should have instance without any subviews after removing them with `removeSubviews`', function () {
+		var view = new View();
 		view.removeSubviews();
 		assert.equal(Object.keys(view.subviews).length, 0);
+		view.remove();
 	});
 
 	it('should have instance with 2 subviews after adding them again', function () {
+		var view = new View();
 		view.addSubview(new View());
 		view.addSubview(new View());
 		assert.equal(Object.keys(view.subviews).length, 2);
+		view.remove();
 	});
 
 	it('should have instance where subview should render it’s subview placeholder with `getRenderPlaceholder`', function () {
+		var view = new View();
 		view.addSubview(new View({
 			tagName: 'p',
 			className: 'subviewPlaceholder'
 		}), 'subviewPlaceholder');
 		assert.equal(view.getSubview('subviewPlaceholder').getRenderPlaceholder(), '<div data-view-cid="' + view.getSubview('subviewPlaceholder').cid + '"></div>');
+		view.remove();
 	});
 
 	it('should have instance which should properly render subview content with `assignSubview`', function () {
+		var view = new View();
+		view.addSubview(new View({
+			tagName: 'p',
+			className: 'subviewPlaceholder'
+		}), 'subviewPlaceholder');
 		view.render = function () {
 			this.$el.html(view.getSubview('subviewPlaceholder').getRenderPlaceholder());
 			view.assignSubview('subviewPlaceholder');
 			return this;
 		};
 		assert.equal($(view.render().el).find('p.subviewPlaceholder').length, 1);
+		view.remove();
 	});
 
 	it('should have instance without any subviews after removing them with `remove`', function () {
+		var view = new View();
 		view.remove();
 		assert.equal(view.subviews, undefined); // eslint-disable-line no-undefined
 	});
--- test/virtual-dom.js
+++ test/virtual-dom.js
@@ -4,7 +4,6 @@ var View = require('../virtual-dom');
 
 describe('Virtual DOM', function () {
 
-	var view, viewFromTemplate;
 	var count = 0;
 
 	function template ( cnt ) {
@@ -12,37 +11,39 @@ describe('Virtual DOM', function () {
 	}
 
 	it('should have instance with `renderDiff` method', function () {
-		view = new View();
+		var view = new View();
 		assert.equal(typeof view.renderDiff, 'function');
+		view.remove();
 	});
 
 	it('should have instance which should have "span.test" element with "0" as content after render diffing', function () {
+		var view = new View();
 		var $el = $(template(count));
 		view.renderDiff($el);
 		assert.equal(view.$el.find('.test').length, 1);
 		assert.equal(view.$el.find('.test').text(), '0');
 		assert.equal(view.$el.find('.test1').length, 0);
+		view.remove();
 	});
 
 	it('should have instance which should have "span.test" element with "2" as content after subsequent render diffing', function () {
+		var view = new View();
 		var $el;
 		count++;
 		count++;
 		$el = $(template(count));
 		view.renderDiff($el);
 		assert.equal(view.$el.find('.test').text(), '2');
+		view.remove();
 	});
 
 	it('should have instance which should render it’s view fully from template if `fromTemplate` is set to true', function () {
-		viewFromTemplate = new View({ fromTemplate: true });
-		viewFromTemplate.renderDiff(template(count));
+		var view = new View({ fromTemplate: true });
+		view.renderDiff(template(count));
 		count++;
-		viewFromTemplate.renderDiff(template(count));
-		assert.equal(viewFromTemplate.$el.is('span.test'), true);
-		assert.equal(viewFromTemplate.$el.text(), '3');
-	});
-
-	after(function () {
+		view.renderDiff(template(count));
+		assert.equal(view.$el.is('span.test'), true);
+		assert.equal(view.$el.text(), '3');
 		view.remove();
 	});
 
